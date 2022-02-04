1bf78909c3d9365959e3316e28a98a0090048ae8
Tati <86420885+Tati-Ramos@users.noreply.github.com>
2022-02-04 09:05:29 -0300

    ocultador de arquivos

---
+++ ocultador/oa.py
@@ -0,0 +1,18 @@
+# ctypes é uma biblioteca de funções externas para Python.
+# Ela fornece tipos de dados compatíveis com C e permite funções de chamada em DLLs ou bibliotecas compartilhadas. Ela pode ser
+# usada para agrupar essas bibliotecas em Python puro.
+
+import ctypes
+
+pasta = input("Digite o caminho da pasta a ser ocultada ex (C:/pasta): ")
+
+atributo_ocultar = 0x02 #hexadecimal
+
+retorno = ctypes.windll.kernel32.SetFileAttributesW(pasta, atributo_ocultar)
+
+if retorno:
+    print("Arquivo foi ocultado")
+else:
+    print("Arquivo não foi ocultado")
+
+
