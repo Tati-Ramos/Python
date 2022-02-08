83b159fa061dbe0d8f6282f5610ec387ff1ab60c
Tati <86420885+Tati-Ramos@users.noreply.github.com>
2022-02-08 16:15:17 -0300

    banco de dados com SQLAlchemy, Rest Api com persistência em banco de dados e Rest Api com autenticação

--- api_atividades/utils.py
+++ api_atividades/utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 # Insere dados na tabela pessoa
 def insere_pessoas():
@@ -25,8 +25,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Rafael').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
-    #insere_pessoas()
-    #altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    insere_usuario('rafael', '123')
+    insere_usuario('tati', '321')
+    consulta_todos_usuarios()
+    # insere_pessoas()
+    # altera_pessoa()
+    # exclui_pessoa()
+    # consulta_pessoas()
